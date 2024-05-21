3b7918ab24c354688904fe36fb289488fe26a899
Satyarth Praveen <satyarth934@gmail.com>
2024-05-20 18:00:36 -0700

    Added code to skip globus transfer call if there are no new files to export. Added code for export cleanup.

--- src/exporting.py
+++ src/exporting.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 import util
 import util_constants as c
 import transfer
+import computing as cmp
 
 import logging
 import logger_config
@@ -24,7 +25,6 @@ def export_data(
         dst_path,
         authorizer,
         check_prev_task=True,
-        delete_source_on_successful_transfer=False,
         label=None,
 ):
     exp_logger.info("Initiating import transfer...")
@@ -40,37 +40,78 @@ def export_data(
             exp_logger.debug("Terminating the process to wait for compute process to finish.")
             sys.exit("Waiting for compute process to complete!")
 
+        # Get the number of new files processed in the last COMPUTE run
+        processed_filepaths = cmp.get_processed_filepaths()
+        exp_logger.info(f"{len(processed_filepaths)} files processed in the last COMPUTE run.")
+        
     # Transferring data
     # -------------------------------------------------
-    exp_result = transfer.transfer_data(
-        src_endpoint_uuid=src_endpoint_uuid,
-        dst_endpoint_uuid=dst_endpoint_uuid,
-        src_path=src_path,
-        dst_path=dst_path,
-        transfer_client=transfer_client,
-        label=label,
-    )
-
-    # Updating the .last_state file
-    util.update_last_state(c.STATES.EXPORT)
-
-    # Updating the .last_task_id file
-    util.update_last_taskid(exp_result['task_id'])
-
-    # Delete all files in the src_path!!!
-    # FIXIT: NOTE: ERROR: PROBLEM: SOLVE: TODO:
-    # This does not work because the files are deleted as soon as the task is submitted. 
-    # So the files are deleted even before the transfer task can take place!!! 
-    # So I need to submit this as another globus task. 
-    # This "state" can be "EXPORT_CLEANUP" and the "taskid" can be updated as well to make sure all the files are cleared before the next of tasks!!!
-    if delete_source_on_successful_transfer:
-        exp_logger.debug("Deleting the input files in compute src")
-        exp_logger.warning("Assuming that the src_path for export exists on the same machine as the compute machine!")
-
-        with os.scandir(src_path) as path_files:
-            for path_file in path_files:
-                # path_file_path = f"{src_path}/{path_file}"
-                if path_file.is_file():
-                    util.delete_file(path_file.path, sleep_time=0)
+    # Check if there are any files to export
+    files_to_export = os.listdir(src_path)
+    if len(files_to_export) <= 0:
+        exp_logger.debug("No new files to export. Terminating process!")
+        exp_result = None
+    else:
+        exp_result = transfer.transfer_data(
+            src_endpoint_uuid=src_endpoint_uuid,
+            dst_endpoint_uuid=dst_endpoint_uuid,
+            src_path=src_path,
+            dst_path=dst_path,
+            transfer_client=transfer_client,
+            label=label,
+        )
 
     return exp_result
+
+
+def export_cleanup(
+        dir_path,
+        authorizer, 
+        prev_task_id=True,
+):
+    exp_logger.info("Initiating export cleanup...")
+
+    # Check previous task status
+    # -------------------------------------------------
+    if prev_task_id is not None:
+        # Initializing TransferClient
+        transfer_client = globus_sdk.TransferClient(authorizer=authorizer)
+
+        # prev_task_details = transfer_client.get_task(prev_task_id)
+        prev_task_details = transfer.get_task_stats(
+            task_id=prev_task_id, 
+            transfer_client=transfer_client,
+        )
+        exp_logger.info(f"Prev Task ID: {prev_task_id} has status: {prev_task_details['status']}")
+        exp_logger.info(f"Prev Task details: {prev_task_details}")
+
+        if prev_task_details['status'] in ['ACTIVE', 'FAILED']:
+            # close the program and wait for the next cronjob call hoping that the function would be completed by then.
+            msg = f"Prev Task is still ACTIVE. Terminating the current run to wait for previous process to complete."
+            exp_logger.warning(msg)
+            sys.exit(msg)
+            
+        elif prev_task_details['status'] == 'SUCCEEDED':
+            # continue with the program.
+            pass
+    
+    # Delete the processed files in compute destination
+    # -------------------------------------------------
+    exp_logger.debug("Deleting the processed files in compute dst")
+    try:
+        util.touch(path=c.EXPORT_CLEANUP_FLAG_FILE)
+
+        with os.scandir(dir_path) as path_files:
+            for path_file in path_files:
+                if path_file.is_file():
+                    util.delete_file(path_file.path, sleep_time=0)
+        
+        exp_logger.debug("Export cleanup complete.")
+        
+    except Exception as e:
+        raise e
+    
+    finally:
+        util.delete_file(c.EXPORT_CLEANUP_FLAG_FILE, sleep_time=2)
+        exp_logger.debug("Deleted the export cleanup flag file.")
+    
